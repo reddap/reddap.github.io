e637267f8c0e2636de2eb6a594dead4698386305
reddap <70059776+reddap@users.noreply.github.com>
2021-11-24 14:12:36 -0300

    Delete extension-for-chrome directory

--- extension-for-chrome/content.js
+++
@@ -1,4 +0,0 @@
-var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://reddap.github.io/gatric-public/trash/src/script.js';
-    document.body.appendChild(script);
