5727e37188150f2a3f5f293a9673bbebd2b7ca3e
reddap <70059776+reddap@users.noreply.github.com>
2021-11-24 13:48:14 -0300

    Update ping_gartic.js

--- gatric-public/trash/ping_gartic.js
+++ gatric-public/trash/ping_gartic.js
@@ -1,36 +1,17 @@
-var i = 0;
-while(i==0){
-
-var start = new Date();
-var a;
-var b;
-var img = new Image();
-
-img.onerror = function() {
-    a=new Date() - start
-};
-
-img.src = 'https://www.gartic.com.br/';
-
-
-window.fetch = (function(fetch) {
-  return function(fn, t) {
-    const begin = Date.now();
-    return fetch.apply(this, arguments).then(function(response) {
-      response.timing = Date.now() - begin;
-      return response;
-    });
-  };
-})(window.fetch);
-
-fetch('https://gartic.com.br')
-  .then(function(response) {
-   b=response.timing
-    return response.json();
-  })
-
-document.querySelectorAll('input')[7].attributes[0].textContent = parseInt((a+b)/2)+'ms'
-
-
- await new Promise(wait => setTimeout(wait, 5000));
+'use strict';
+async function ping() {
+  let start = Date.now();
+  let promise = await fetch("https://gartic.com.br/");
+  let rightValue = Date.now();
+  return `${rightValue - start}ms`;
+  ping().then(async(animate_param) => {
+    console.log(animate_param);
+  });
 }
+setInterval(() => {
+  ping().then((b) => {
+    if (document.location.href.includes("https://gartic.com.br/0") == true) {
+      document.querySelectorAll("input")[8].attributes[0].textContent = b;
+    }
+  });
+}, 3000);
